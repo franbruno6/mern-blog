a4ece19eed7e9ad0e88c1355c49771a073c35d34
Francisco Bruno <franbruno99@gmail.com>
2024-04-20 18:08:40 -0300

    add dashboard overview to admin

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from "flowbite-react";
-import { HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
+import { HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -10,13 +10,6 @@ export default function DashSidebar() {
     const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
     const [ tab, setTab ] = useState('');
-    useEffect(() => {
-        const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
-        if (tabFromUrl) {
-        setTab(tabFromUrl);
-        }
-    }, [location.search])
 
     const handleSignout = async () => {
         try {
@@ -35,10 +28,31 @@ export default function DashSidebar() {
         }
     };
 
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get('tab');
+        if (tabFromUrl) {
+        setTab(tabFromUrl);
+        }
+    }, [location.search]);
+
     return (
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
                 <Sidebar.ItemGroup className="flex flex-col gap-1">
+                    {
+                        currentUser.isAdmin && (
+                            <Link to='/dashboard?tab=dash'>
+                                <Sidebar.Item 
+                                    active={tab === 'dash' || !tab} 
+                                    icon={HiChartPie}
+                                    as='div'
+                                >
+                                    Dashboard
+                                </Sidebar.Item>
+                            </Link>
+                        )
+                    }
                     <Link to='/dashboard?tab=profile'>
                         <Sidebar.Item 
                             active={tab === 'profile'} 
