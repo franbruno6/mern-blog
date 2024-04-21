2c292bb962cb99b50a3bf13c99cb51a984d4ac59
Francisco Bruno <franbruno99@gmail.com>
2024-04-21 11:02:21 -0300

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,22 @@
 export default function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font-semibold text-center my-7">About Francisco's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Francisco's Blog is a blog site that I created based on a tutorial. I am a Systems Analyst and I am learning how to create web applications using React and Node.js. 
+            </p>
+            <p>
+              I created this blog site to practice my skills and to share my knowledge with others. I hope you enjoy reading my blog posts and find them helpful. 
+            </p>
+            <p>
+              If you have any questions or comments, please feel free to contact me. Thank you for visiting my blog!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
